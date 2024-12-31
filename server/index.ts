e6249f3d91d0ffb68b09398e6a5d454bc248eb36
shimizu <noon3m.op.sh@gmail.com>
2025-01-01 00:56:43 +0900

    [add]すべてのルートにCORS設定を追加

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
+import { cors } from 'hono/cors';
 
 const app = new Hono();
 
@@ -10,3 +11,15 @@ export const customLogger = (message: string, ...rest: string[]) => {
 
 // ログの設定
 app.use('*', logger(customLogger));
+
+// すべてのルートにCORS設定を適用
+app.use(
+  '*',
+  cors({
+    origin: '*',
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowHeaders: ['Content-Type', 'Authorization'],
+    exposeHeaders: ['Content-Length'],
+    maxAge: 600,
+  }),
+);
